97ec1e7b0ec95219969376af1dd6ad21e8fedd25
ndresgarc <email@andresgarcia.dev>
2022-03-10 21:18:55 +0100

    chore: storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.js",
+    "../src/**/*.stories.mdx"
   ],
   "addons": [
     "@storybook/addon-links",
