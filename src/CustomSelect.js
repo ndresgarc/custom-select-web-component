a1fcfa2134ce9717e2fb4b257ccec9bb7e6ea75c
ndresgarc <email@andresgarcia.dev>
2022-03-15 21:12:38 +0100

    feat: add button css vars

--- src/CustomSelect.js
+++ src/CustomSelect.js
@@ -17,7 +17,11 @@ export const defineCustomSelect = function () {
             }
 
             .cs-button {
-
+                background: var(--cs-button--background, transparent);
+                border: var(--cs-button--background, 1px solid #ccc);
+                border-radius: var(--cs-button--border-radius, none);
+                color: var(--cs-button--color, black);
+                padding: var(--cs-button--padding, black);
             }
 
             .cs-options {
