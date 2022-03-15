eb7c8780a5a0f9de0333341cf8761232f6680b41
ndresgarc <email@andresgarcia.dev>
2022-03-15 21:34:07 +0100

    chore: code formatting

--- src/CustomSelect.stories.js
+++ src/CustomSelect.stories.js
@@ -4,17 +4,20 @@ defineCustomSelect();
 
 function createCustomSelect(props) {
     return `
+
         <style>
             :root {
                 --cs-button--background: ${props.css_button_background};
             }
         </style>
+
         <custom-select>
             <option value="1">${props.label}</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
             <option value="4">Four</option>
         </custom-select>
+        
     `;
 }
 
@@ -25,21 +28,18 @@ export default {
     }
 };
 
-// More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
 const Template = ({ label, ...args }) => {
-    // You can either use a function to create DOM elements or use a plain html string!
-    // return `<div>${label}</div>`;
     return createCustomSelect({ label, ...args });
-  };
-  
-  export const Blue = Template.bind({});
-  Blue.args = {
+};
+
+export const Blue = Template.bind({});
+Blue.args = {
     css_button_background: 'blue',
     label: 'Button blue',
-  };
-  
-  export const Red = Template.bind({});
-  Red.args = {
+};
+
+export const Red = Template.bind({});
+Red.args = {
     css_button_background: 'red',
     label: 'Button red',
-  };
+};
