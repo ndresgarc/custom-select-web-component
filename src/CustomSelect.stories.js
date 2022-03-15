d800eeaf4f5dd1a1cf460b1cf7afa38df70f0389
ndresgarc <email@andresgarcia.dev>
2022-03-15 21:33:18 +0100

    test: add sb stories for background color

--- src/CustomSelect.stories.js
+++ src/CustomSelect.stories.js
@@ -2,14 +2,44 @@ import { defineCustomSelect } from './CustomSelect';
 
 defineCustomSelect();
 
+function createCustomSelect(props) {
+    return `
+        <style>
+            :root {
+                --cs-button--background: ${props.css_button_background};
+            }
+        </style>
+        <custom-select>
+            <option value="1">${props.label}</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+            <option value="4">Four</option>
+        </custom-select>
+    `;
+}
+
 export default {
-    title: 'CCT/two'
+    title: 'CUSTOM SELECT',
+    argTypes: {
+        css_button_background: { control: 'text' }
+    }
 };
 
-export const customSelect = () => `
-    <custom-select>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-        <option value="4">Four</option>
-    </custom-select>`;
+// More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
+const Template = ({ label, ...args }) => {
+    // You can either use a function to create DOM elements or use a plain html string!
+    // return `<div>${label}</div>`;
+    return createCustomSelect({ label, ...args });
+  };
+  
+  export const Blue = Template.bind({});
+  Blue.args = {
+    css_button_background: 'blue',
+    label: 'Button blue',
+  };
+  
+  export const Red = Template.bind({});
+  Red.args = {
+    css_button_background: 'red',
+    label: 'Button red',
+  };
