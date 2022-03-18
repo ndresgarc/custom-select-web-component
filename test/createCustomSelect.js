37fbb43b51a9eda677c8ee7fde37f182b6ab507b
ndresgarc <email@andresgarcia.dev>
2022-03-18 23:03:26 +0100

    test: update story for disabled attribute

--- test/createCustomSelect.js
+++ test/createCustomSelect.js
@@ -3,6 +3,7 @@ import { defineCustomSelect } from "../src/defineCustomSelect";
 defineCustomSelect();
 
 export const createCustomSelect = function (props) {
+
     return `
 
         <style>
@@ -11,7 +12,9 @@ export const createCustomSelect = function (props) {
             }
         </style>
 
-        <custom-select>
+        <custom-select 
+            ${props.html_disabled ? 'disabled' : ''}
+            ${props.html_open ? 'open' : ''}>
             <option value="1">${props.label}</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
--- test/stories/attributes.stories.js
+++ test/stories/attributes.stories.js
@@ -3,8 +3,13 @@ import { createCustomSelect } from '../createCustomSelect';
 export default {
     title: 'Custom Select / Attributes',
     argTypes: {
-        open: {
-            control: 'boolean'
+        html_disabled: {
+            control: 'boolean',
+            name: 'disabled',
+        },
+        html_open: {
+            control: 'boolean',
+            name: 'open',
         }
     }
 };
@@ -15,5 +20,10 @@ const Template = ({ label, ...args }) => {
 
 export const Open = Template.bind({});
 Open.args = {
-    open: true
+    html_open: true
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    html_disabled: true
 };
